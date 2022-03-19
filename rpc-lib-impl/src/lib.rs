1b72579d3d9d66c07ae8661d14b74f4854ccf38c
Philipp Fensch <philipp.fensch@rwth-aachen.de>
2022-03-19 14:20:50 +0100

    Added Dual-License MIT + Apache-2

--- rpc-lib-impl/src/lib.rs
+++ rpc-lib-impl/src/lib.rs
@@ -1,3 +1,11 @@
+// Copyright 2022 Philipp Fensch
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 extern crate pest;
 extern crate proc_macro;
 extern crate quote;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+// Copyright 2022 Philipp Fensch
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! # Rpc-lib
 //!
 //! Rpc-lib is a crate to compile an RPC-Definition in the ONC-RPC format ([`RFC 5531`]) into Rust-code. It comes
